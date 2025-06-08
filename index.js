658ec4361c1d37ad10a8c8180944070ac6b89b48
KingOfficer Bots <developement.kingofficerbots@gmail.com>
2025-06-08 23:21:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Customize your bot's status by forking this."];
+const statusMessages = [".gg/YREGhpdGbJ","Watching T2D"];
 
 
 let currentIndex = 0;
